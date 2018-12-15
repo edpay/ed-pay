6c6ad9ab618b1a7feff416bb5fddb7a6f0c7c7e8
soumyag25 <soumyagulapannavar@gmail.com>
2018-12-16 03:14:05 +0530

    institutions page partial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
+import { InstitutionsComponent} from './components/institutions/institutions.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'about', component: AboutComponent}
+  { path: 'about', component: AboutComponent},
+  { path: 'institutions', component: InstitutionsComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { SearchboxComponent } from './components/searchbox/searchbox.component';
+import { InstitutionsComponent } from './components/institutions/institutions.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { SearchboxComponent } from './components/searchbox/searchbox.component';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    SearchboxComponent
+    SearchboxComponent,
+    InstitutionsComponent
   ],
   imports: [
     BrowserModule,
